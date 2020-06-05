068fc23bb20ba8e6c4c70717be110c7d92f8f6f7
liyinhao <liyinhao@sensetime.com>
2020-06-05 15:14:46 +0800

    add formating

--- pathseg/datasets/pipelines/__init__.py
+++ pathseg/datasets/pipelines/__init__.py
@@ -1,7 +1,9 @@
 from .data_augmentation import Flip, RandomRotate90, ShiftScaleRotate
+from .formating import Formating
 from .loading import Loading
 from .sampling import RandomSampling
 
 __all__ = [
-    'Loading', 'Flip', 'ShiftScaleRotate', 'RandomRotate90', 'RandomSampling'
+    'Loading', 'Flip', 'ShiftScaleRotate', 'RandomRotate90', 'RandomSampling',
+    'Formating'
 ]

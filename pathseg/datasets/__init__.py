563b861d9c00bb4ff0d5d36e3b87bfcb9528b593
yinchimaoliang <liyinhao0413@qq.com>
2020-06-06 20:10:58 +0800

    add dataloader

--- pathseg/datasets/__init__.py
+++ pathseg/datasets/__init__.py
@@ -1,1 +1,4 @@
 from pathseg.datasets.builder import DATASETS  # noqa: F401
+from .base_dataset import BaseDataset
+
+__all__ = ['BaseDataset']
--- pathseg/datasets/base_dataset.py
+++ pathseg/datasets/base_dataset.py
@@ -1,8 +1,9 @@
 import os
 
+import numpy as np
 from torch.utils.data import Dataset
 
-from pathseg.datasets import DATASETS
+from .builder import DATASETS
 from .pipelines import Compose
 
 
@@ -19,9 +20,10 @@ class BaseDataset(Dataset):
         self.pipeline = Compose(pipeline)
         self.test_mode = test_mode
         self.classes = classes
-        self.img_paths, self.ann_paths = self.load_data(self.data_root)
+        self.img_paths, self.ann_paths = self._load_data(self.data_root)
+        self._set_group_flag()
 
-    def load_data(self, data_root):
+    def _load_data(self, data_root):
         names = os.listdir(os.path.join(data_root, 'images'))
         img_paths = [os.path.join(data_root, 'images', name) for name in names]
         ann_paths = [
@@ -29,27 +31,40 @@ class BaseDataset(Dataset):
         ]
         return img_paths, ann_paths
 
-    def get_data_info(self, idx):
+    def _get_data_info(self, idx):
         img_path = self.img_paths[idx]
         ann_path = self.ann_paths[idx]
 
         input_dict = dict(img_path=img_path, ann_path=ann_path)
         return input_dict
 
-    def prepare_train_data(self, idx):
-        input_dict = self.get_data_info(idx)
+    def _prepare_train_data(self, idx):
+        input_dict = self._get_data_info(idx)
         example = self.pipeline(input_dict)
         return example
 
-    def prepare_test_data(self, idx):
-        input_dict = self.get_data_info(idx)
+    def _prepare_test_data(self, idx):
+        input_dict = self._get_data_info(idx)
         example = self.pipeline(input_dict)
         return example
+
+    def _set_group_flag(self):
+        """Set flag according to image aspect ratio.
+
+        Images with aspect ratio greater than 1 will be set as group 1,
+        otherwise group 0.
+        In 3D datasets, they are all the same, thus are all zeros
+
+        """
+        self.flag = np.zeros(len(self), dtype=np.uint8)
 
     def __getitem__(self, idx):
         if self.test_mode:
-            sample = self.prepare_test_data(idx)
+            sample = self._prepare_test_data(idx)
         else:
-            sample = self.prepare_train_data(idx)
+            sample = self._prepare_train_data(idx)
 
         return sample
+
+    def __len__(self):
+        return len(self.img_paths)

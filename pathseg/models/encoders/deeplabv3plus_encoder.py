1ec30de955e1bf3b53a3c469d3704dba5ca2f5f2
liyinhao <liyinhao@sensetime.com>
2020-07-21 17:43:52 +0800

    Add deeplabv3plusencoder

---
+++ pathseg/models/encoders/deeplabv3plus_encoder.py
@@ -0,0 +1,32 @@
+import segmentation_models_pytorch as smp
+
+from pathseg.core.utils.dilate_utils import replace_strides_with_dilation
+from ..builder import ENCODERS
+from .base_encoder import BaseEncoder
+
+model = smp.DeepLabV3Plus(encoder_weights=None)
+print(model)
+
+
+@ENCODERS.register_module()
+class DeeplabV3PlusEncoder(BaseEncoder):
+
+    def __init__(self, backbone, encoder_output_stride=16):
+        super().__init__(backbone)
+        if encoder_output_stride == 8:
+            self.make_dilated(stage_list=[3, 4], dilation_list=[2, 4])
+
+        elif encoder_output_stride == 16:
+            self.make_dilated(stage_list=[4], dilation_list=[2])
+        else:
+            raise ValueError(
+                'Encoder output stride should be 8 or 16, got {}'.format(
+                    encoder_output_stride))
+
+    def make_dilated(self, stage_list, dilation_list):
+        stages = self.backbone.stages
+        for stage_indx, dilation_rate in zip(stage_list, dilation_list):
+            replace_strides_with_dilation(
+                module=stages[stage_indx],
+                dilation_rate=dilation_rate,
+            )
--- tests/test_models/test_encoders.py
+++ tests/test_models/test_encoders.py
@@ -1,4 +1,4 @@
-from pathseg.models import UnetEncoder, build_encoder
+from pathseg.models import DeeplabV3PlusEncoder, UnetEncoder, build_encoder
 
 
 def test_unet_encoder():
@@ -9,3 +9,15 @@ def test_unet_encoder():
     unet_encoder = build_encoder(cfg)
 
     assert isinstance(unet_encoder, UnetEncoder)
+
+
+def test_deeplabv3plus_encoder():
+    backbone = dict(type='ResNet', name='resnet18', weights='imagenet')
+    cfg = dict(
+        type='DeeplabV3PlusEncoder',
+        backbone=backbone,
+        encoder_output_stride=16)
+    deeplabv3plus_encoder = build_encoder(cfg)
+    assert isinstance(deeplabv3plus_encoder, DeeplabV3PlusEncoder)
+    assert deeplabv3plus_encoder.backbone.layer4[0].conv1.dilation == (2, 2)
+    assert deeplabv3plus_encoder.backbone.layer4[0].conv2.dilation == (2, 2)

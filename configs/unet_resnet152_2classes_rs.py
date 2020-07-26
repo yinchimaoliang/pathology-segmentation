d98011c80e9de43640e3466953eaf2e1362d0213
liyinhao <liyinhao@sensetime.com>
2020-07-26 14:58:40 +0800

    Add confings for deeplabv3+ and unet

---
+++ configs/unet_resnet152_2classes_rs.py
@@ -0,0 +1,83 @@
+model = dict(
+    type='UNet',
+    encoder=dict(
+        type='UnetEncoder',
+        backbone=dict(type='ResNet', name='resnet152', weights='imagenet'),
+    ),
+    decoder=dict(
+        type='UnetDecoder',
+        decoder_channels=(512, 256, 128, 64, 64),
+        final_channels=2))
+
+data = dict(
+    class_names=['Lesions'],
+    samples_per_gpu=10,
+    workers_per_gpu=4,
+    train=dict(
+        type='BaseDataset',
+        data_root='./data/train',
+        pipeline=[
+            dict(
+                type='RandomSampling', prob_global=.5,
+                target_shape=(512, 512)),
+            dict(
+                type='Flip',
+                prob=.5,
+                flip_ratio_horizontal=.5,
+                flip_ratio_vertical=.5),
+            dict(type='ShiftScaleRotate', prob=.5),
+            dict(type='RandomRotate90', prob=.5),
+            dict(
+                type='Formating',
+                mean=[0.5, 0.5, 0.5],
+                std=[0.1, 0.1, 0.1],
+                num_classes=2)
+        ],
+        random_sampling=True,
+        width=512,
+        height=512,
+        stride=512,
+        repeat=100),
+    valid=dict(
+        type='BaseDataset',
+        data_root='./data/valid',
+        pipeline=[
+            dict(
+                type='Formating',
+                mean=[0.5, 0.5, 0.5],
+                std=[0.1, 0.1, 0.1],
+                num_classes=2)
+        ],
+        random_sampling=False,
+        width=512,
+        height=512,
+        stride=512,
+    ),
+    test=dict(
+        type='BaseDataset',
+        data_root='./data/test',
+        pipeline=[
+            dict(
+                type='Formating',
+                mean=[0.5, 0.5, 0.5],
+                std=[0.1, 0.1, 0.1],
+                num_classes=2)
+        ],
+        random_sampling=False,
+        width=512,
+        height=512,
+        stride=512,
+    ))
+
+train = dict(
+    loss=dict(type='BCEDiceLoss'),
+    optimizer=dict(type='Adam', lr=0.01, weight_decay=0.0001),
+    scheduler=dict(step_size=30, gamma=0.1))
+
+valid = dict(evals=['Dsc', 'Iou'])
+
+test = dict(colors=[[0, 255, 0]], weight=0.2, evals=['Dsc', 'Iou'])
+
+log_level = 'INFO'
+
+dist_params = dict(backend='nccl')

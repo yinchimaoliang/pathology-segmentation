81a424cf789e466c0e6fbd267217e4fb77ca82f4
liyinhao <liyinhao@sensetime.com>
2020-09-23 11:17:37 +0800

    Change annotations making.

--- pathseg/datasets/pipelines/loading.py
+++ pathseg/datasets/pipelines/loading.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 from pathseg.datasets.builder import PIPELINES
 
@@ -6,22 +7,28 @@ from pathseg.datasets.builder import PIPELINES
 @PIPELINES.register_module()
 class Loading(object):
 
-    def __init__(self, shape):
+    def __init__(self, shape, num_class):
         self.shape = shape
+        self.num_class = num_class
 
     def __call__(self, results):
         img_path = results['img_path']
         ann_path = results['ann_path']
 
         img = cv.imread(img_path)
-        ann = cv.imread(ann_path, 0)
+        ann_pixel = cv.imread(ann_path, 0)
 
         if self.shape is not None:
             img = cv.resize(img, self.shape)
-            ann = cv.resize(ann, self.shape)
+            ann_pixel = cv.resize(ann_pixel, self.shape)
 
+        ann = []
+        for i in range(self.num_class):
+            ann.append(
+                np.sum(ann_pixel == i + 1) /
+                (ann_pixel.shape[0] * ann_pixel.shape[1]))
         results['image'] = img
-        results['annotation'] = ann
+        results['annotation'] = np.array(ann)
         return results
 
     def __repr__(self):

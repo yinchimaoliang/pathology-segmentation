420eee94842016690381bcbc15546bce18d3de59
liyinhao <liyinhao@sensetime.com>
2020-11-14 19:26:27 +0800

    Update pipeline

--- pathseg/datasets/pipelines/__init__.py
+++ pathseg/datasets/pipelines/__init__.py
@@ -1,10 +1,14 @@
 from .compose import Compose
-from .data_augmentation import Flip, RandomRotate90, ShiftScaleRotate
-from .formating import Formating
-from .loading import Loading
-from .sampling import RandomSampling
+from .formating import (Collect, ImageToTensor, ToDataContainer, ToTensor,
+                        Transpose, to_tensor)
+from .loading import LoadAnnotations, LoadImageFromFile
+from .test_time_aug import MultiScaleFlipAug
+from .transforms import (Normalize, Pad, PhotoMetricDistortion, RandomCrop,
+                         RandomFlip, Resize, SegRescale)
 
 __all__ = [
-    'Loading', 'Flip', 'ShiftScaleRotate', 'RandomRotate90', 'RandomSampling',
-    'Formating', 'Compose'
+    'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
+    'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
+    'MultiScaleFlipAug', 'Resize', 'RandomFlip', 'Pad', 'RandomCrop',
+    'Normalize', 'SegRescale', 'PhotoMetricDistortion'
 ]

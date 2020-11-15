6d74b206aa996962efd4f3b5fb21caa054b517bd
liyinhao <liyinhao@sensetime.com>
2020-11-15 10:23:08 +0800

    Add ce loss.

--- pathseg/models/losses/__init__.py
+++ pathseg/models/losses/__init__.py
@@ -1,5 +1,6 @@
 from .bce_dice_loss import BCEDiceLoss
 from .bce_loss import BCELoss
+from .cross_entropy_loss import CrossEntropyLoss
 from .dice_loss import DiceLoss
 
-__all__ = ['BCELoss', 'DiceLoss', 'BCEDiceLoss']
+__all__ = ['BCELoss', 'DiceLoss', 'BCEDiceLoss', 'CrossEntropyLoss']
